4014714113a9aaa1c650ff911a8a6e5293646a61
yicone <yicone@gmail.com>
2023-11-28 17:49:07 +0800

    Introducing CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,40 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "@unocss/astro";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://yic.one",
   integrations: [
     mdx(),
     UnoCSS(),
     sitemap(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        collections: [
+          {
+            layout: "$/layouts/BlogPost.astro",
+            name: "posts",
+            label: "Blog Posts",
+            folder: "src/content/blog",
+            create: true,
+            delete: true,
+            fields: [
+              { name: "title", widget: "string", label: "Title" },
+              { name: "description", widget: "string", label: "Description" },
+              { name: "pubDate", widget: "datetime", label: "Publish Date" },
+              { name: "heroImage", widget: "image", label: "Hero Image" },
+              { name: "tags", widget: "list", label: "Tags" },
+              { name: "body", widget: "markdown", label: "Body" },
+            ],
+          },
+        ],
+      },
+    }),
   ],
 });
