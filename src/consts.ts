19c19ceb8ae2663e7d33240a827e2e961a050551
yicone <yicone@gmail.com>
2024-09-12 18:25:24 +0800

    Update contents

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Yes, I Code one!';
+export const SITE_TITLE = 'Yes, I Coded one!';
 export const SITE_DESCRIPTION = 'Welcome to yicone\'s homepage!';
